14625e7d9c4339b357bb299909b06258c124d74e
mjingram <micha.j.ingram@gmail.com>
2021-08-17 16:28:43 -0500

    testing user entity

--- JPARainbowBeat/src/main/java/com/skilldistillery/rainbowbeat/entities/User.java
+++ JPARainbowBeat/src/main/java/com/skilldistillery/rainbowbeat/entities/User.java
@@ -1,6 +1,53 @@
 package com.skilldistillery.rainbowbeat.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String username;
+	
+	private String password;
 
+	public User() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
+	}
+	
+	
 }
