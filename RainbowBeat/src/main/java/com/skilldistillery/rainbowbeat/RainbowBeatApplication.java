74575ca15d56269a5f004d4785139bc56dcb9d68
Caleb Koch <caleb.koch@gmail.com>
2021-08-17 14:17:18 -0600

    Stb Spring Boot/REST project

---
+++ RainbowBeat/src/main/java/com/skilldistillery/rainbowbeat/RainbowBeatApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.rainbowbeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainbowBeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainbowBeatApplication.class, args);
+	}
+
+}
---
+++ RainbowBeat/src/test/java/com/skilldistillery/rainbowbeat/RainbowBeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.rainbowbeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainbowBeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
