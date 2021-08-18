19dcd3dd1f7809e84278082372e8bee26ee71e9d
Gabriel Avila <gabriel.avila.ga09@gmail.com>
2021-08-18 14:23:10 -0500

    added User model/service in angular

---
+++ ngRainbowBeat/src/app/models/user.ts
@@ -0,0 +1,39 @@
+export class User {
+  id: number;
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  createdAt: string;
+  updatedAt: string;
+  email: string;
+  imageUrl: string;
+  enabled: boolean;
+  role: string;
+
+  constructor(
+    id: number =0,
+    username: string = '',
+    password: string = '',
+    firstName: string = '',
+    lastName: string = '',
+    createdAt: string = '',
+    updatedAt: string = '',
+    email: string = '',
+    imageUrl: string = '',
+    enabled: boolean = true,
+    role: string = ''
+  ) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.email = email;
+    this.imageUrl = imageUrl;
+    this.enabled = enabled;
+    this.role = role;
+  }
+}
