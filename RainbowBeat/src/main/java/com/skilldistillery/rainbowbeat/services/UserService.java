12b3b35b5dfbf13f26c925b9892442ed8248b7fe
Gabriel Avila <gabriel.avila.ga09@gmail.com>
2021-08-17 19:19:08 -0500

    created User API endpoints

--- RainbowBeat/src/main/java/com/skilldistillery/rainbowbeat/services/UserService.java
+++ RainbowBeat/src/main/java/com/skilldistillery/rainbowbeat/services/UserService.java
@@ -1,8 +1,16 @@
 package com.skilldistillery.rainbowbeat.services;
 
+import java.util.List;
+
 import com.skilldistillery.rainbowbeat.entities.User;
 
 public interface UserService {
 	
+	List<User> allUsers();
+	
 	User userByUsername(String username);
+	
+	User updateUser(User user);
+	
+	User createUser(User user);
 }
--- RainbowBeat/src/main/java/com/skilldistillery/rainbowbeat/services/UserServiceImpl.java
+++ RainbowBeat/src/main/java/com/skilldistillery/rainbowbeat/services/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.rainbowbeat.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +18,20 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public User userByUsername(String username) {
 		return userRepo.findByUsername(username);
+	}
+
+	@Override
+	public List<User> allUsers() {
+		return userRepo.findAll();
+	}
+
+	@Override
+	public User updateUser(User user) {
+		return userRepo.saveAndFlush(user);
+	}
+
+	@Override
+	public User createUser(User user) {
+		return userRepo.saveAndFlush(user);
 	}
 }
