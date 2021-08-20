dd531c65375fe9d96194cb8ac418eec0c389783c
Caleb Koch <caleb.koch@gmail.com>
2021-08-19 18:09:53 -0600

    retrieve comments by user logic is added

--- ngRainbowBeat/src/app/components/admin/admin.component.ts
+++ ngRainbowBeat/src/app/components/admin/admin.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
+import { PostService } from 'src/app/services/post.service';
+import { Post } from 'src/app/models/post';
+import { CommentService } from 'src/app/services/comment.service';
 
 
 @Component({
@@ -13,13 +16,19 @@ export class AdminComponent implements OnInit {
 
   constructor(
     private userService: UserService,
-    private authService: AuthService) { }
+    private authService: AuthService,
+    private postService: PostService,
+    private commentService: CommentService
+    ) { }
 
     users: User [] = [];
     user: User | null = null;
     editUser: User | null = null;
     public encoded = this.authService.getCredentials();
   public decoded = atob((this.encoded ?? 'null'));
+
+  posts: Post [] = [];
+  comments: Comment[] = [];
 
   ngOnInit(): void {
     this.getUserInfo();
@@ -49,6 +58,30 @@ export class AdminComponent implements OnInit {
       }
     );
   }
+
+  displayUserPosts(username: string){
+  this.postService.postsByUsername(username).subscribe(
+    posts => {
+      this.posts = posts;
+      console.log(posts);
+    },
+    noPosts => {
+      console.log('admin.component.displayUserPosts() could not obtain posts');
+    }
+  );
+  }
+
+  displayUserComments(username: string){
+    this.commentService.commentsByUsername(username).subscribe(
+      comments => {
+        this.comments = comments;
+        console.log(comments);
+      },
+      noComments => {
+        console.log('admin.component.displayUserPosts() could not obtain posts');
+      }
+    );
+    }
 
 }
 
