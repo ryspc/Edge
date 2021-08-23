5b87cdf71e91bc4561170d81172a9b6d0ee8cd1d
Ray S <rayspace@protonmail.com>
2021-08-23 10:40:32 -0600

    rating progress

--- RainbowBeat/src/main/java/com/skilldistillery/rainbowbeat/services/RatingServiceImpl.java
+++ RainbowBeat/src/main/java/com/skilldistillery/rainbowbeat/services/RatingServiceImpl.java
@@ -6,16 +6,23 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.skilldistillery.rainbowbeat.entities.Comment;
 import com.skilldistillery.rainbowbeat.entities.Post;
 import com.skilldistillery.rainbowbeat.entities.Rating;
+import com.skilldistillery.rainbowbeat.entities.RatingId;
+import com.skilldistillery.rainbowbeat.entities.User;
+import com.skilldistillery.rainbowbeat.repositories.PostRepository;
 import com.skilldistillery.rainbowbeat.repositories.RatingRepository;
+import com.skilldistillery.rainbowbeat.repositories.UserRepository;
 
 @Service
 public class RatingServiceImpl implements RatingService {
 	
 	@Autowired
 	private RatingRepository ratingRepo;
+	@Autowired
+	private PostRepository postRepo;
+	@Autowired
+	private UserRepository userRepo;
 
 	@Override
 	public Rating show(int id) {
@@ -43,12 +50,20 @@ public class RatingServiceImpl implements RatingService {
 	}
 	
 	@Override
-	public Rating create(Rating rating) {
-		
+	public Rating create(Rating rating,String username, int postId) {
+		User user = userRepo.findByUsername(username);
+		Post post = postRepo.queryById(postId);
+		if(user != null && post != null) {
+			RatingId ratingId = new RatingId(postId, user.getId());
+			rating.setId(ratingId);
 		try {
+			rating.setUser(user);
+			rating.setPost(post);
 			ratingRepo.saveAndFlush(rating);
-		} catch (Exception e) {System.out.println(e);}
-		return rating;
+			return rating;
+		} catch (Exception e) {System.out.println(e);}}
+		
+		return null;
 	}
 
 
