a3f3b9b4c649901d70e0e344f78f169a9d47746e
Caleb Koch <caleb.koch@gmail.com>
2021-08-23 14:35:33 -0600

    forced commit for pull of rating update

--- ngRainbowBeat/src/app/components/home/home.component.ts
+++ ngRainbowBeat/src/app/components/home/home.component.ts
@@ -104,7 +104,7 @@ export class HomeComponent implements OnInit {
                post.ratings = data;
                console.log(post.id);
                console.log(this.ratings.length);
-               
+
               // this.ratingTotal = this.ratings.length;
               // console.log('rating updated');
               // this.ratings.forEach(rating => {
@@ -129,24 +129,24 @@ export class HomeComponent implements OnInit {
   }
 
   like(post: Post) {
-    if(this.loggedInUser && this.rating){
-      this.rating.rating = true;
-      this.rating.user = this.loggedInUser;
-      this.rating.post = post;
-      console.log(this.rating.rating);
-      console.log(this.rating.user);
-      console.log(this.rating.post);
+  //   if(this.loggedInUser && this.rating){
+  //     this.rating.rating = true;
+  //     this.rating.user = this.loggedInUser;
+  //     this.rating.post = post;
+  //     console.log(this.rating.rating);
+  //     console.log(this.rating.user);
+  //     console.log(this.rating.post);
 
-      this.ratingService.create(this.rating).subscribe(
-        update => {
-          console.log('rating created')
-          this.loadPosts();
-        },
-        err => {
-          console.log(err);
-        }
-      );
-    }
+  //     this.ratingService.create(this.rating).subscribe(
+  //       update => {
+  //         console.log('rating created')
+  //         this.loadPosts();
+  //       },
+  //       err => {
+  //         console.error(err);
+  //       }
+  //     );
+  //   }
   }
 
 
